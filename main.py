eb9b185bb1b19b9c5e6b3973db75dccbdc83ea38
MAudrry <audry.munezero.am@gmail.com>
2023-05-19 00:09:55 +0200

    Q1.1 Affichage des éléments de la liste

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ if __name__ == '__main__':
     # Q1.Création d'une liste
     voitures=["Vanguard","Mitsubishi","T.I","Probox","MacLaren","Mercedes","Allione","Rav 4","Porshe","Ferarri"]
     # Fin de la création de la liste
+
+    # Q1.1 Affichage des éléments de la liste
+
+    print("\nQ1.1 Affichage des elements de la liste")
+    print("-------------------------------------------\n")
+    for voiture in voitures:
+        print(voiture)
+
+    # Fin de l'affichage des éléments de la liste
