05866561141dac303d34b19708998744d2b61510
MAudrry <audry.munezero.am@gmail.com>
2023-05-19 00:07:10 +0200

    Q1.Création d'une liste

---
+++ main.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    # Q1.Création d'une liste
+    voitures=["Vanguard","Mitsubishi","T.I","Probox","MacLaren","Mercedes","Allione","Rav 4","Porshe","Ferarri"]
+    # Fin de la création de la liste
